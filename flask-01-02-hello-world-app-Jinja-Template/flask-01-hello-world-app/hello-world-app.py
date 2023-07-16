ec2bf05148fb8f39d52e35cd7b8fb551ac79a8cd
obz10050 <obz10050@gmail.com>
2023-07-16 02:09:08 -0400

    values_updated

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def home():
     return "hello from flask!"
 
-# @app.route("/profile/<string:user>")
-# def profile(user):
-#     return "hello" + user
+@app.route("/profile/<string:user>")
+def profile(user):
+    return "hello" + user
 
 if __name__== "__main__":
-     app.run(host="0.0.0.0", port=4000)
+     app.run(host="0.0.0.0", port=3000)
